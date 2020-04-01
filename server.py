8fec244b51a449720e966a24048129c144daccc1
Chua Choon Hoe <choonhoe@gmail.com>
2020-04-01 17:25:39 +0800

    add store working

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import peeweedbevolve
-from flask import Flask, render_template, request
-from models import db
+from flask import Flask, render_template, request, redirect, flash, url_for
+from models import *
 
 app = Flask(__name__)
 
@@ -20,6 +20,22 @@ def migrate():
 @app.route("/")
 def index():
    return render_template('index.html')
+
+@app.route("/store")
+def store():
+    return render_template('store.html')
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    store_name = request.form.get('store_name')
+    # s = Store.create(name=store_name)
+    try:
+        Store.create(name=store_name)
+        #flash("Store created")
+        return redirect(url_for('store'))
+    except:
+        #flash("Store name exists!")
+        return redirect(url_for('store'))
 
 if __name__ == '__main__':
    app.run()
